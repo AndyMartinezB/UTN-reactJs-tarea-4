3ad7f63f6ce92d5ec37df3d5ede2602ea5da64ca
Andy Martinez Bologna <andres.bologna@mi.unc.edu.ar>
2025-12-26 01:13:33 -0300

    feat: Personalizo tarjetas

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,31 @@ import "@/App.css";
 import Tarjeta from "@/components/Tarjeta/Tarjeta.jsx";
 
 function App() {
-  const nombre = "Andy";
 
-  // function Saludo() {
-  //   return <h2>¡Hola, {nombre.toUpperCase()}!</h2>;
-  // }
 
   return (
     <div className="App">
       <header>
-        <h1>Primer proyecto react</h1>
+        <h1>Nuestro equipo</h1>
       </header>
       <div className="contenedor-tarjetas">
         <Tarjeta
-          imagen="https://cdn-icons-png.freepik.com/256/12631/12631417.png?semt=ais_white_label"
-          nombre="Usuario"
+          imagen="https://img.freepik.com/foto-gratis/programador-smiley-tiro-medio-interiores_23-2149101154.jpg?semt=ais_hybrid&w=740&q=80"
+          nombre="Camilo Pueblo"
           profesion="Desarrollador Web"
-          descripcion="Esta es la descripcion de la tarjeta"
+          descripcion="Especialista en crear experiencias digitales interactivas, fluidas y optimizadas."
         />
         <Tarjeta
-          imagen="https://cdn-icons-png.freepik.com/256/12631/12631417.png?semt=ais_white_label"
-          nombre="Usuario"
-          profesion="Desarrollador Web"
-          descripcion="Esta es la descripcion de la tarjeta"
+          imagen="https://img.freepik.com/foto-gratis/asistente-medico-preparandose-ejercicios-fisioterapia_23-2149071431.jpg?semt=ais_hybrid&w=740&q=80"
+          nombre="Julieta Marra"
+          profesion="Analista funcional"
+          descripcion="Experta en transformar necesidades de negocio en soluciones técnicas claras."
         />
         <Tarjeta
-          imagen="https://cdn-icons-png.freepik.com/256/12631/12631417.png?semt=ais_white_label"
-          nombre="Usuario"
-          profesion="Desarrollador Web"
-          descripcion="Esta es la descripcion de la tarjeta"
+          imagen="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?q=80&w=256&h=256&fit=crop"
+          nombre="Giuliana Pierre"
+          profesion="Ingeniera de software"
+          descripcion="Enfocada en el diseño de arquitecturas robustas y código de alta calidad."
         />
       </div>
     </div>
